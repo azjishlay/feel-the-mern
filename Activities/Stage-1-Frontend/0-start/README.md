7884069b9899a8f714f14b64f39f1e538e5dd1b5
lijasminej <li.jasmine.j@gmail.com>
2017-04-24 00:24:10 -0400

    Add Stage 1 starter files

---
+++ Activities/Stage-1-Frontend/0-start/README.md
@@ -0,0 +1,9 @@
+# My Full Stack Todo App
+
+### Starter files
+
+- - -
+
+### Copyright
+
+Coding Boot Camp © 2017. All Rights Reserved.
---
+++ Activities/Stage-1-Frontend/0-start/src/TodoApp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TodoApp extends Component {
+  render() {
+    return (
+      <div className="main">
+        <h2>My Full Stack Todo App</h2>
+      </div>
+    );
+  }
+}
+
+export default TodoApp;
